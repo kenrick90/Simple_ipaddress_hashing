34823faa1a145b01a8c2d8e7d79e96f9605cabac
Ng, Kenrick (SG Novena) <Kenrick.Ng@baesystems.com>
2020-01-23 17:57:51 +0800

    Full release

--- masking.py
+++ masking.py
@@ -1,58 +1,63 @@
 import re
 import os
-import sys
 import string
 import shutil
 import argparse
-
-def mask_ip(path):
-    
+def mask_ip(inFile, outFile, macAddress):
     #This is the mapping if the ip digits to alphabets, 0 -> 'a' , 1 -> 'b' ...
     ip_mapping=dict(zip(range(0,10),string.ascii_lowercase))
-    f = open(path, "r")
-    nf = open(path + "-sanitised", "w")
-    
+    f = open(inFile, "r")
+    nf = open(outFile, "w")
     line = f.readline()
     while line:
-        wordsInALine = line.split()
-        newwords = []
-        for word in wordsInALine:
-            # check IP address in ipv4 format  and replace them
-            if re.search(r"[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*", word):
-                matchlist = re.findall(r"[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*", word)
-                print(matchlist)
-                for match in matchlist:
-                    print(match)
-                    m = re.search(match, word)
-                    print(str(match),m.start,m.end)
-                    for i in range(m.start(),m.end()):
-                        #word is not [1,9,2,.,1,6,8,.,0,.,1]
-                        word = list(word)
-                        word_original = word[:]
-                        try:
-                            word[i]=ip_mapping[int(word_original[i])]
-                        except:
-                            continue
-                    word = "".join(word)
-            # check mac address and replace them
-            # word = re.sub(r"[0-9a-zA-Z][0-9a-zA-Z][-:][0-9a-zA-Z][0-9a-zA-Z][-:]"
-            #               r"[0-9a-zA-Z][0-9a-zA-Z][-:][0-9a-zA-Z][0-9a-zA-Z][-:]"
-            #               r"[0-9a-zA-Z][0-9a-zA-Z][-:][0-9a-zA-Z][0-9a-zA-Z]", "xx:xx:xx:xx:xx:xx", word)
-            newwords.append(word)
-        newwords = ' '.join(newwords)
-        nf.write(newwords + "\n")
+        # check IP address in ipv4 format  and replace them
+        if re.search(r"[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*", line):
+            matchlist = re.findall(r"[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*", line)
+            for match in matchlist:
+                m = re.search(match, line)
+                #Only tweak the characters which are the IP address and not other parts of the line
+                for i in range(m.start(),m.end()):
+                    line = list(line)
+                    line_original = line[:]
+                    try:
+                        line[i]=ip_mapping[int(line_original[i])]
+                    except:
+                        continue
+                line = "".join(line)
+        # check mac address and replace them
+        if macAddress == True:
+            line = re.sub(r"[0-9a-zA-Z][0-9a-zA-Z][-:][0-9a-zA-Z][0-9a-zA-Z][-:]"
+                          r"[0-9a-zA-Z][0-9a-zA-Z][-:][0-9a-zA-Z][0-9a-zA-Z][-:]"
+                          r"[0-9a-zA-Z][0-9a-zA-Z][-:][0-9a-zA-Z][0-9a-zA-Z]", "xx:xx:xx:xx:xx:xx", line)
+        nf.write(line)
         line = f.readline()
     f.close()
     nf.close()
 
 if __name__=='__main__':
-    parser = argparse.ArgumentParser(description='Directory to Sanitise')
-    parser.add_argument('indir', type=str, help='Input dir for Sanitisation')
-    parser.add_argument('outdir', type=str, help='Output dir for Sanitisation')
+    parser = argparse.ArgumentParser(description='Mask IP address of input file by writing a output file with the IP address changed')
+    parser.add_argument('-R', '--recursive', action='store_true',
+                        help="recursively sanitised entire directory and its files")
+    parser.add_argument('-m', '--macAddress', action='store_true',
+                        help="If enabled, mask mac address into xx:xx:xx:xx:xx:xx, default is not enabled")
+    parser.add_argument('input',type=str, help='Input directory OR file for masking')
+    parser.add_argument('output',type=str, help='Output directory or file for masking')
     args = parser.parse_args()
-    shutil.copytree(args.indir,args.outdir)
-    for root, dirs, files in os.walk(args.outdir, topdown=True):
-        for filename in files:
-            fullpath = os.path.join(root,filename)
-            mask_ip(fullpath)
-            os.remove(fullpath)
+
+    #create a separate folder for this tasks
+    if args.recursive:
+        try:
+            shutil.copytree(args.input,args.output)
+        except:
+            print "Output directory already exist or no permission! Please try again with another " \
+                  "output directory name or delete existing output directory folder."
+            quit()
+        for root, dirs, files in os.walk(args.output, topdown=True):
+            for filename in files:
+                filepath = os.path.join(root, filename)
+                newfilepath = os.path.join(root,"masked-"+filename)
+                mask_ip(filepath,newfilepath,args.macAddress)
+                os.remove(filepath)
+        quit()
+    mask_ip(args.input, args.output, args.macAddress)
+
---
+++ new.txt.lol
@@ -0,0 +1,7 @@
+bcd.bcd.bcd.bcd
+bcd.bcd.bcd.bcd,123.123
+bcd.bcd.bcd.dcb,bcd.dcb.ede.fdf
+00:0a:95:9d:68:16
+00:0a:95:9d:68:16,bcd.bcd.bcd.bcd
+bcd.bcd.bcd.bcd
+
